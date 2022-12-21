532117cbffba02d68ad17c10273d8cb8f961969b
Diego <diego_sousa.s@outlook.com>
2022-12-20 21:23:43 -0300

    Criando repositório

--- Program.cs
+++ Program.cs
@@ -33,3 +33,18 @@ public class Product {
     public string Name { get; set; }
     public string Code { get; set; }
 }
+
+public static class ProductRepository {
+    private static List<Product> Products { get;set; }
+
+    public static void Add(Product product) {
+        if(Products == null)
+            Products = new List<Product>();
+
+        Products.Add(product);
+    }
+
+    public static Product GetBy(string code){
+        return Products.First(p => p.Code == code);
+    }
+}
